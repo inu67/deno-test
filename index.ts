f130fbcd5046702eda37d9f2af0ae23fe4fb27ff
inu67 <146059255+inu67@users.noreply.github.com>
2023-12-25 10:31:59 +0000

    fix

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
-import { logger, poweredBy } from 'https://deno.land/x/hono@v3.11.7/middleware.ts'
 import { Hono } from 'https://deno.land/x/hono@v3.11.7/mod.ts'
+
 
 const app = new Hono()
 
-app.use('*', logger(), poweredBy())
 app.get('/', (c) => {
   return c.text('Hello Deno!')
 })
 
+app.get('/wappasutei',(c)={
+  return c.text('fuck ctkp')
+})
 Deno.serve(app.fetch)
