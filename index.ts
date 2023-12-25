49b624fb8bc880e14a9e7235c0ccb8ee31442fd0
inu67 <146059255+inu67@users.noreply.github.com>
2023-12-25 11:28:34 +0000

    fix

--- index.ts
+++ index.ts
@@ -2,9 +2,7 @@ import { Hono } from 'https://deno.land/x/hono@v3.11.10/mod.ts'
 import { serveStatic } from 'https://deno.land/x/hono/middleware.ts'
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Deno!')
-})
+app.get('/', serveStatic({ path: './static/jojo.html' }))
 
-app.get('/wappasutei',serveStatic({ path: './static/jojo.html' }))
+app.get('/wappasutei',(c) => c.text('Hello! Hono!'))
 Deno.serve(app.fetch)
